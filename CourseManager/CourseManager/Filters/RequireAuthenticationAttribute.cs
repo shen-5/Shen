0f8fecf76fd7a1638f838ed3ad2b9d72a9455fd9
123 <123>
2019-06-30 17:02:11 +0800

    11.18

---
+++ CourseManager/CourseManager/Filters/RequireAuthenticationAttribute.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CourseManager.Models.ValidatableObjects;
+using CourseManager.Models;
+
+namespace CourseManager.Filters
+{
+    public class RequireAuthenticationAttribute : FilterAttribute, IAuthorizationFilter
+    {
+        public void OnAuthorization(AuthorizationContext filterContext)
+        {
+            if (filterContext.HttpContext.Session != null)
+            {
+                //TODO
+                var Suser = filterContext.HttpContext.Session["user"];
+                var user = "";
+                if (Suser == null)
+                {
+                    user = " ";
+                }
+                else
+                {
+                    user = Suser.ToString();
+                }
+                if (!string.IsNullOrWhiteSpace(user))
+                {
+                    return;
+                }
+
+                var cookie = filterContext.HttpContext.Request.Cookies["user"];
+                var temp = "";
+                if (cookie == null)
+                {
+                    temp = " ";
+                }
+                else
+                {
+                    temp = cookie.ToString();
+                }
+                if (string.IsNullOrWhiteSpace(temp))
+                {
+                    throw new UnauthorizedException();
+                }
+             
+                var content = temp.DecryptQueryString();
+                CourseManagerEntities db = new CourseManagerEntities();
+                if (!db.Users.Any(u => u.Account == content))
+                {
+                    throw new UnauthorizedException();
+                }
+
+            }
+        }
+    }
+}
---
+++ CourseManager/CourseManager/Models/CourseManagementExtention.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CourseManager.Models
+{
+    public partial class CourseManagements
+    {
+        [Display(Name = "班主任")]
+        public string TeacherName 
+        {
+            get {
+                CourseManagerEntities db = new CourseManagerEntities();
+                var teacher = db.Teachers.Where(t => t.Id == TeacherId).FirstOrDefault();
+                if (teacher == null) {
+                    return "";
+                }
+                return teacher.Name;
+            }
+        
+        }
+
+        [Display(Name = "班级")]
+        public string ClassName
+        {
+            get
+            {
+
+                CourseManagerEntities db = new CourseManagerEntities();
+                var class_ = db.Classes.Where(t => t.Id == ClassId).FirstOrDefault();
+                if (class_ == null)
+                {
+                    return "";
+                }
+                return class_.Name;
+            }
+
+        }
+
+        [Display(Name = "课程")]
+        public string CourseName
+        {
+            get
+            {
+
+                CourseManagerEntities db = new CourseManagerEntities();
+                var course = db.Courses.Where(t => t.Id == CourseId).FirstOrDefault();
+                if (course == null)
+                {
+                    return "";
+                }
+                return course.Name;
+            }
+
+        }
+    }
+}
